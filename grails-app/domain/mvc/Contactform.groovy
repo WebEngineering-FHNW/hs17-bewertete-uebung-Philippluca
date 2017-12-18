85f178f92e04ef6dd7ad47125cd5ef540945c99c
Philipp Lüthi <philipp@luethi.rocks>
2017-12-18 21:11:47 +0100

    Corrected failing validation

--- grails-app/domain/mvc/Contactform.groovy
+++ grails-app/domain/mvc/Contactform.groovy
@@ -7,8 +7,8 @@ class Contactform {
     String message
 
     static constraints = {
-        name(nullable: false, maxSize: 60)
-        email(nullable: false, maxSize: 70,email: true)
+        name(nullable: false,blank: false, maxSize: 60)
+        email(nullable: false, blank:false, maxSize: 70,email: true)
         message(nullable: false, blank: false, maxSize: 1200)
     }
 }
